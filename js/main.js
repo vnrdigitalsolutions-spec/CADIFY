597acf717f90030c7bede652efc905e160018677
vnrdigitalsolutions-spec <vnrdigitalsolutions@gmail.com>
2026-01-22 14:51:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const sections = [
+  "hero",
+  "logos",
+  "features",
+  "courses",
+  "cta"
+];
+
+const app = document.getElementById("app");
+
+sections.forEach(section => {
+  fetch(`sections/${section}.html`)
+    .then(res => res.text())
+    .then(html => {
+      app.insertAdjacentHTML("beforeend", html);
+    });
+});
 
